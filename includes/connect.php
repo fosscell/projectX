693c776b6c100c07462333781e8c878e856379a6
saba <sabahath_05@yahoo.co.in>
2013-10-31 20:19:51 +0530

    made connect.php

---
+++ includes/connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$user="b110048cs";
+$pass="b110048cs";
+$db="db_b110048cs";
+
+
+$mysqli=mysqli_connect("localhost",$user,$pass,$db) or die("connection failed". mysqli_error());
+
+
+?>
+
+
